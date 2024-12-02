5384dd184838d4b983896f34f41dcded9235986c
djawad0 <dania.jawad@codesorbit.com>
2024-12-02 13:17:49 +0500

    Setup webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
+const isPublicRoute = createRouteMatcher(["/api/webhooks/clerk"]);
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
